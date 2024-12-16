e2c11b98513ac8a185dadad81002545d539c79a4
Coimbra <adsjkbd@gmail.com>
2024-12-16 05:53:07 -0300

    endpoint findAll para users

--- src/main/java/com/jkbd/api/api/service/UserService.java
+++ src/main/java/com/jkbd/api/api/service/UserService.java
@@ -2,6 +2,9 @@ package com.jkbd.api.api.service;
 
 import com.jkbd.api.api.entity.Users;
 
+import java.util.List;
+
 public interface UserService {
     Users findById(Integer id);
+    List<Users> findAll();
 }
